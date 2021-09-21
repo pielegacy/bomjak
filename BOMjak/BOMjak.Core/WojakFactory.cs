788ff21322d8300d38e09c4972a5b54bbe08a281
Alex <alex_billson@outlook.com>
2021-09-21 12:30:20 +1000

    TF2 update

--- BOMjak/BOMjak.Core/WojakFactory.cs
+++ BOMjak/BOMjak.Core/WojakFactory.cs
@@ -13,8 +13,9 @@ namespace BOMjak.Core
     {
         private const int TransparencySize = 384;
         private const int TransparencyOffsetX = 140;
-        private const int TransparencyOffsetY = 20;
+        private const int TransparencyOffsetY = 60;
         private const string ForegroundPath = "Resources/foreground.png";
+        private const string BackgroundPath = "Resources/background.png";
 
         public const int ImageWidth = 800;
         public const int ImageHeight = 633;
@@ -22,6 +23,7 @@ namespace BOMjak.Core
         public static async Task<Image> CreateImageDefaultAsync(IEnumerable<string> layerPaths)
         {
             using var foreground = await Image.LoadAsync<Rgba32>(ForegroundPath);
+            using var background = await Image.LoadAsync<Rgba32>(BackgroundPath);
 
             var layerImages = await Task.WhenAll(layerPaths.Select((layerPath) => Image.LoadAsync<Rgba32>(layerPath)));
 
@@ -29,6 +31,7 @@ namespace BOMjak.Core
 
             result.Mutate((ctx) =>
             {
+                ctx.DrawImage(background, 1);
                 foreach (var layerImage in layerImages)
                 {
                     layerImage.Mutate((lCtx) =>
@@ -49,12 +52,14 @@ namespace BOMjak.Core
         public static async Task<Image> CreateImageDynamicAsync(string layerPath)
         {
             using var foreground = await Image.LoadAsync<Rgba32>(ForegroundPath);
+            using var background = await Image.LoadAsync<Rgba32>(BackgroundPath);
             using var layerImage = await Image.LoadAsync<Rgba32>(layerPath);
 
             var result = new Image<Rgba32>(ImageWidth, ImageHeight, Rgba32.ParseHex("fff"));
 
             result.Mutate((ctx) =>
             {
+                ctx.DrawImage(background, 1);
                 layerImage.Mutate((lCtx) =>
                 {
                     lCtx.ResizeToTransparency(layerImage);
